89c2a2103e0c6808207a61442eb94f6ce71a986a
donghyunKang-dev <make_w@naver.com>
2022-04-15 22:04:12 +0900

    [강동현] Header.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import Grid from '../../elements/Grid';
 
 const Header = (props) => {
-
     return(
         <>
             <Grid width="1050px" margin="0 auto">
@@ -38,12 +37,27 @@ const Header = (props) => {
 
                 <Gnb>
                     <GnbMenu>
-                        <li>전체 카테고리</li>
-                        <li>신상품</li>
-                        <li>베스트</li>
-                        <li>알뜰쇼핑</li>
-                        <li>특가/혜택</li>
+                        <li className="menu1">
+                            <span className='all-category'/>
+                            전체 카테고리
+                        </li>
+                        <li className="menu2">신상품</li>
+                        <li className="menu3">베스트</li>
+                        <li className="menu4">알뜰쇼핑</li>
+                        <li className="menu5">특가/혜택</li>
                     </GnbMenu>
+                    <Grid position="relative">
+                        <GnbInput placeholder='검색어를 입력해주세요.' />
+                        <GnbInputClick
+                            src="https://res.kurly.com/pc/service/common/1908/ico_search_x2.png"
+                            width="30px"
+                            height="30px"
+                            alt="마켓컬리 검색창 돋보기"
+                        />
+                    </Grid>
+                    <Icons className='location' /> 
+                    <Icons className='pick' />
+                    <Icons className='cart' />
                 </Gnb>
             </Grid>
         </>
@@ -61,15 +75,15 @@ const HeaderWrap = styled.div`
 const HeaderMenu = styled.ul`
 	display: flex;
 	font-size: 12px;
-	
-    & li {
+	font-family: 'Noto Sans';
+    & li{
 		margin: 0px 0px 0px 24px;
 		cursor: pointer;
 		list-style: none;
         position: relative;
         line-height: 37px;
 
-		&.menu:before {
+		&.menu:before{
             content: "";
             float: right;
             width: 1px;
@@ -96,6 +110,7 @@ const Logo = styled.div`
 const Gnb = styled.div`
     display: flex;
     align-items: center;
+    padding: 15px 0 0 0;
     justify-content: space-between;
     background-color: #ffffff;
     letter-spacing: -0.3px;
@@ -103,11 +118,78 @@ const Gnb = styled.div`
 
 const GnbMenu = styled.ul`
     display: flex;
+    justify-content: space-between;
 	font-size: 16px;
     color: #333;
     line-height: 20px;
     text-align: center;
+    font-family: 'Noto Sans';
+    & li{
+        list-style: none;
+        cursor: pointer;
+        font-weight: 700;
+        
+        &:hover {
+			color: #5f0081;
+		}
+        &.menu1{
+            width: 148px;
+        }
+        &.menu2, &.menu4{
+            width: 124px;
+        }
+        &.menu3, &.menu5{
+            width: 116px;
+        };
+    };
+    & span {
+		content: url('https://res.kurly.com/pc/service/common/1908/ico_gnb_all_off.png');
+		position: relative;
+		width: 16px;
+        height: 14px;
+        top: 1px;
+        left: -16px;
+	};
+`;
 
+const GnbInput = styled.input`
+    width: 242px;
+    height: 36px;
+    padding: 0 60px 0 14px;
+    border: 1px solid #f7f7f6;
+    border-radius: 18px;
+    background-color: #f7f7f7;
+    font-family: 'Noto Sans';
+    font-weight: 400;
+    font-size: 12px;
+    color: #666;
+    line-height: 16px;
+    outline: none;
+    &::placeholder{
+        letter-spacing: -.05em;
+    };
+`;
+
+const GnbInputClick = styled.img`
+    position: absolute;
+    right: 5px;
+    top: 3px;
+    cursor: pointer;
+`;
+
+const Icons = styled.span`
+    width: 36px;
+	height: 36px;
+    cursor: pointer;
+    &.location{
+        background: url("https://res.kurly.com/pc/ico/2008/ico_delivery_setting.svg?ver=1") no-repeat 50% 50%;
+    };
+    &.pick{
+        background: url('https://res.kurly.com/pc/service/pick/btn-heart-off.svg') no-repeat 50% 50%;
+    };
+    &.cart{
+        background: url('https://res.kurly.com/pc/service/common/2011/ico_cart.svg') no-repeat 50% 50%;
+    };
 `;
 
 export default Header;
