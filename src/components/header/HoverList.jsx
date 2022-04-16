2a59e9f6a07ea7ebf40896d46580e6084308ac67
donghyunKang-dev <make_w@naver.com>
2022-04-17 01:06:27 +0900

    [강동현] Add react-slick to Banner.jsx

---
+++ src/components/header/HoverList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HoverList = (props) => {
+    return (
+        <Container>
+            <ul>
+                <li>공지사항</li>
+                <li>자주하는 질문</li>
+                <li>1:1 문의</li>
+                <li>대량주문 문의</li>
+                <li>상품제안</li>
+                <li>에코포장 피드백</li>
+            </ul>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: none;
+    position: absolute;
+    z-index: 10000000;
+    right: 2px;
+    top: 34px;
+    width: 102px;
+    padding: 3px 9px;
+    border: 1px solid #ddd;
+    background-color: #fff;
+
+    ul li {
+        padding: 0;
+        font-size: 12px;
+        color: #404040;
+        line-height: 24px;
+        white-space: nowrap;
+        cursor: pointer;
+    }
+`;
+export default HoverList;
---
+++ src/shared/banner.js
@@ -0,0 +1,24 @@
+export const RESP = {
+    imaUrlList: [
+        {
+            imgUrl: 'https://img-cf.kurly.com/banner/main/pc/img/33729ca6-1a24-40d0-9f55-454c9955d267',
+            alt: '배너 1',
+        },
+        {
+            imgUrl: 'https://img-cf.kurly.com/banner/main/pc/img/fa8ecd2d-b420-4805-9d9a-d158caa7c8a4',
+            alt: '배너 2',
+        },
+        {
+            imgUrl: 'https://img-cf.kurly.com/banner/main/pc/img/46b0eed9-0c77-4731-829b-f6f32727a2c3',
+            alt: '배너 3',
+        },
+        {
+            imgUrl: 'https://img-cf.kurly.com/banner/main/pc/img/2fc6b874-164b-41bc-9d09-e3fcf9e4c804',
+            alt: '배너 4',
+        },
+        {
+            imgUrl: 'https://img-cf.kurly.com/banner/main/pc/img/ea170c0a-7890-4f99-9c09-9cc7a3bacd74',
+            alt: '배너 5',
+        },
+    ]
+}
