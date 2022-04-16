2cede8997aa0673b5eae229a882ed79a6b8ccfea
donghyunKang-dev <make_w@naver.com>
2022-04-16 22:03:38 +0900

    [강동현] 문단 처리

--- src/components/cart/OrderInfo.jsx
+++ src/components/cart/OrderInfo.jsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const OrderInfo = (props) => {
     return (
         <Container>
-            <Text>{`· [배송준비중] 이전까지 주문 취소 가능합니다.`}</Text>
-            <Text>{`· [마이컬리 > 주문내역 상세페이지]에서 직접 취소하실 수 있습니다.`}</Text>
+            <Text><span>·</span>{`[배송준비중] 이전까지 주문 취소 가능합니다.`}</Text>
+            <Text><span>·</span>{`[마이컬리 > 주문내역 상세페이지]에서 직접 취소하실 수 있습니다.`}</Text>
         </Container>
     )
 }
@@ -16,12 +16,19 @@ const Container = styled.div`
 `;
 
 const Text = styled.span`
+    position: relative;
     display: block;
     position: relative;
-    padding: 6px 0 0 0;
+    padding: 6px 0 0 10px;
     font-size: 12px;
     color: #666;
     line-height: 18px;
     letter-spacing: -.5px;
+    white-space: pre-wrap;
+
+    & span{
+        position: absolute;
+        left: 0;
+    }
 `;
 export default OrderInfo;
