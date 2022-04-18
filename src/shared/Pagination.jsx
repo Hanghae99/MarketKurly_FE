9d4b22e89bae3115614b81bc62b74dd2e589bf37
donghyunKang-dev <make_w@naver.com>
2022-04-18 23:20:29 +0900

    ...

--- src/shared/Pagination.jsx
+++ src/shared/Pagination.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Grid } from '../elements';
 import { useDispatch, useSelector } from 'react-redux';
+import { actionCreators as productActions } from '../redux/modules/product';
 
 const Pagination = (props) => {
     const dispatch = useDispatch();
     const page_num = useSelector(state => state.product.page);
     const link_arr = useRef([]);
+    const [ choicedPage, setChoicedPage ] = useState(page_num);
+    const [ prevPage, setPrevPage ] = useState();
 
     {/* 이번 프로젝트에서는 5페이지로 고정 */}
     const num_arr = [1, 2, 3, 4, 5];
 
     useEffect(() => {
-        link_arr.current[page_num-1].className += " choice-page"
-    }, [page_num]);
-
-
+        if(prevPage){
+            link_arr.current[prevPage-1].classList.remove("choice-page");
+            link_arr.current[choicedPage-1].className += " choice-page";
+        } else {
+            link_arr.current[choicedPage-1].className += " choice-page";
+        };
+        setPrevPage(choicedPage);    
+    }, [choicedPage]);
+    console.log(prevPage, choicedPage)
+    
+    const clickBtn = (e) => {
+        const page = e.target.innerText;
+        setChoicedPage(e.target.innerText);
+        dispatch(productActions.getProductApi(page));
+    };
 
     const goods = "meat"
     // console.log(link_arr.current[0].innerText)
@@ -28,19 +42,20 @@ const Pagination = (props) => {
             flex
             justify="center"
         >
-            <StyledLink to={`/${props.page}/goods=${goods}&page=1`} className='layout-pagination-button layout-pagination-first-page'>맨 처음 페이지로 가기</StyledLink>
-            <StyledLink to="/cart" className='layout-pagination-button layout-pagination-prev-page'>이전 페이지로 가기</StyledLink>
+            <StyledLink to='#' className='layout-pagination-button layout-pagination-first-page'>맨 처음 페이지로 가기</StyledLink>
+            <StyledLink to='#' className='layout-pagination-button layout-pagination-prev-page'>이전 페이지로 가기</StyledLink>
             {num_arr.map((v, i) => 
                 <StyledLink 
-                    to="/" 
+                    to="#" 
                     key={i} 
+                    onClick={clickBtn}
                     ref={elem => link_arr.current[i] = elem} 
                     className='layout-pagination-button'
                 >
                     {i+1}
                 </StyledLink>
             )}
-            
+
             {/* 이번 프로젝트에서는 5페이지로 고정 */}
             {/* <StyledLink to="/" ref={elem => link_arr.current[0] = elem} className='layout-pagination-button'>1</StyledLink>
             <StyledLink to="/" ref={elem => link_arr.current[1] = elem} className='layout-pagination-button'>2</StyledLink>
@@ -48,8 +63,8 @@ const Pagination = (props) => {
             <StyledLink to="/" ref={elem => link_arr.current[3] = elem} className='layout-pagination-button'>4</StyledLink>
             <StyledLink to="/" ref={elem => link_arr.current[4] = elem} className='layout-pagination-button'>5</StyledLink> */}
             
-            <StyledLink to="/" className='layout-pagination-button layout-pagination-next-page'>다음 페이지로 가기</StyledLink>
-            <StyledLink to="/" className='layout-pagination-button layout-pagination-last-page'>맨 끝 페이지로 가기</StyledLink>
+            <StyledLink to="#" className='layout-pagination-button layout-pagination-next-page'>다음 페이지로 가기</StyledLink>
+            <StyledLink to="#" className='layout-pagination-button layout-pagination-last-page'>맨 끝 페이지로 가기</StyledLink>
         </Grid>
     );
 };
