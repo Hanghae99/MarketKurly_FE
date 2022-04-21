9d502798a15824a9ecfe0751b5929735a7fe174c
YeonJae <duswo9595@naver.com>
2022-04-21 16:03:52 +0900

    회원가입에러해결& 총합계 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,160 +5,163 @@ const Footer = (props) => {
   return (
     <>
       <Bar />
-      <Container>
-        <Section>
-          <div>
-            <h2
-              style={{
-                fontWeight: "500",
-                fontSize: "20px",
-                color: "#333",
-              }}
-            >
-              고객행복센터
-            </h2>
-            <h1
-              style={{
-                fontWeight: "700",
-                fontSize: "28px",
-                color: "#333",
-                marinTop: "15px",
-                paddingTop: "20px",
-              }}
-            >
-              1644-1107
-            </h1>
-
-            <Button>카카오톡 문의</Button>
-            <Button>1:1 문의</Button>
-            <Button>대량주문 문의</Button>
-          </div>
-
+      <To>
+        <Container>
           <Section>
-            <CenterInfo>
-              <li>365고객센터</li>
-              <li>오전7시 - 오후7시</li>
-              <li>365고객센터</li>
-              <li>오전7시 - 오후7시</li>
-              <li>24시간 접수 가능</li>
-              <li>고객센터 운영시간에 순차적으로 답변해드리게습니다.</li>
-              <li>{""}</li>
-              <li>비회원의 경우 메일로 문의 바랍니다.</li>
-            </CenterInfo>
-          </Section>
-        </Section>
-
-        <Wrap>
-          <Menu>
-            <List>컬리소개</List>
-            <List>컬리소개영상</List>
-            <List>인재채용</List>
-            <List>이용약관</List>
-            <List>개인정보처리방침</List>
-            <List>이용안내</List>
-          </Menu>
-
-          <div style={{ margin: "30px 0 0 10px" }}>
-            <KurlyInfo>
-              <li>
-                법인명 (상호): 주식회사 컬리 | 사업자등록번호:261-81-23567{" "}
-                <span>사업자정보 확인</span>
-              </li>
-              <li>
-                통신판매업:제2018-서울강남-01646 호 | 개인정보보호책임자:이원준
-              </li>
-              <li>
-                주소:서울특별시 강남구 테헤란로113, 18층(역삼동) |
-                대표이사:김슬아
-              </li>
-              <li>
-                입점문의:<span>입점문의하기</span> | 마케팅제휴:{" "}
-                <span>business@kurlycorp.com</span>
-              </li>
-              <li>
-                채용문의:<span>recruit@kurlycorp.com</span>
-              </li>
-              <li>
-                팩스:070-7500-6098 | 이메일:<span>help@kurlycorp.com</span>
-              </li>
-              <li>
-                대량주문 문의 : <span>kurlygift@kurlycorp.com</span>
-              </li>
-            </KurlyInfo>
-
-            <KurlyIcon>
-              <Img
-                src="https://res.kurly.com/pc/ico/1810/ico_instagram.png"
-                alt="isms 로고"
-                width="35px"
-              />
-              <Img
-                src="https://res.kurly.com/pc/ico/1810/ico_fb.png"
-                alt="isms 로고"
-                width="35px"
-              />
-              <Img
-                src="https://res.kurly.com/pc/ico/1810/ico_blog.png"
-                alt="마켓컬리 네이버블로그 바로가기"
-                width="35px"
-              />
-              <Img
-                src="https://res.kurly.com/pc/ico/1810/ico_naverpost.png"
-                alt="isms 로고"
-                width="35px"
-              />
-              <Img
-                src="https://res.kurly.com/pc/ico/1810/ico_youtube.png"
-                alt="마켓컬리 유튜브 바로가기"
-                width="35px"
-              />
-            </KurlyIcon>
-          </div>
-        </Wrap>
-      </Container>
-
-      <Container>
-        <OutsideInfo>
-          <Section>
-            <Img
-              src="https://res.kurly.com/kurly/logo/isms_220310.png"
-              alt="isms 로고"
-              width="35px"
-            />
             <div>
-              [인증범위]마켓컬리 쇼핑몰 서비스 개발 · 운영
-              <br />
-              [유효기간]2022.01.19~2025.01.18
+              <h2
+                style={{
+                  fontWeight: "500",
+                  fontSize: "20px",
+                  color: "#333",
+                }}
+              >
+                고객행복센터
+              </h2>
+              <h1
+                style={{
+                  fontWeight: "700",
+                  fontSize: "28px",
+                  color: "#333",
+                  marinTop: "15px",
+                  paddingTop: "20px",
+                }}
+              >
+                1644-1107
+              </h1>
+
+              <Button>카카오톡 문의</Button>
+              <Button>1:1 문의</Button>
+              <Button>대량주문 문의</Button>
             </div>
+
+            <Section>
+              <CenterInfo>
+                <li>365고객센터</li>
+                <li>오전7시 - 오후7시</li>
+                <li>365고객센터</li>
+                <li>오전7시 - 오후7시</li>
+                <li>24시간 접수 가능</li>
+                <li>고객센터 운영시간에 순차적으로 답변해드리게습니다.</li>
+                <li>{""}</li>
+                <li>비회원의 경우 메일로 문의 바랍니다.</li>
+              </CenterInfo>
+            </Section>
           </Section>
 
-          <Section>
-            <Img
-              src="https://res.kurly.com/pc/ico/2001/logo_eprivacyplus.png"
-              alt="eprivacy plus 로고"
-              width="35px"
-            />
-            <div margin="0px 10px">
-              개인정보보호 우수 웹사이트 ·<br />
-              개인정보처리시스템인증 (ePRIVACY PLUS)
-            </div>
-          </Section>
+          <Wrap>
+            <Menu>
+              <List>컬리소개</List>
+              <List>컬리소개영상</List>
+              <List>인재채용</List>
+              <List>이용약관</List>
+              <List>개인정보처리방침</List>
+              <List>이용안내</List>
+            </Menu>
 
-          <Section>
-            <Img
-              src="https://res.kurly.com/pc/service/main/2009/logo_payments.png"
-              alt="payments 로고"
-              width="100px"
-            />
-            <div margin="0px 10px">
-              고객님의 안전거래를 위해 현금 등으로 결제 시 저희 쇼핑몰에서
-              가입한
-              <br />
-              토스 페이먼츠 구매안전(에스크로)서비스를 이용하실 수 있습니다.
+            <div style={{ margin: "30px 0 0 10px" }}>
+              <KurlyInfo>
+                <li>
+                  법인명 (상호): 주식회사 컬리 | 사업자등록번호:261-81-23567{" "}
+                  <span>사업자정보 확인</span>
+                </li>
+                <li>
+                  통신판매업:제2018-서울강남-01646 호 |
+                  개인정보보호책임자:이원준
+                </li>
+                <li>
+                  주소:서울특별시 강남구 테헤란로113, 18층(역삼동) |
+                  대표이사:김슬아
+                </li>
+                <li>
+                  입점문의:<span>입점문의하기</span> | 마케팅제휴:{" "}
+                  <span>business@kurlycorp.com</span>
+                </li>
+                <li>
+                  채용문의:<span>recruit@kurlycorp.com</span>
+                </li>
+                <li>
+                  팩스:070-7500-6098 | 이메일:<span>help@kurlycorp.com</span>
+                </li>
+                <li>
+                  대량주문 문의 : <span>kurlygift@kurlycorp.com</span>
+                </li>
+              </KurlyInfo>
+
+              <KurlyIcon>
+                <Img
+                  src="https://res.kurly.com/pc/ico/1810/ico_instagram.png"
+                  alt="isms 로고"
+                  width="35px"
+                />
+                <Img
+                  src="https://res.kurly.com/pc/ico/1810/ico_fb.png"
+                  alt="isms 로고"
+                  width="35px"
+                />
+                <Img
+                  src="https://res.kurly.com/pc/ico/1810/ico_blog.png"
+                  alt="마켓컬리 네이버블로그 바로가기"
+                  width="35px"
+                />
+                <Img
+                  src="https://res.kurly.com/pc/ico/1810/ico_naverpost.png"
+                  alt="isms 로고"
+                  width="35px"
+                />
+                <Img
+                  src="https://res.kurly.com/pc/ico/1810/ico_youtube.png"
+                  alt="마켓컬리 유튜브 바로가기"
+                  width="35px"
+                />
+              </KurlyIcon>
             </div>
-          </Section>
-        </OutsideInfo>
-      </Container>
+          </Wrap>
+        </Container>
+
+        <Container>
+          <OutsideInfo>
+            <Section>
+              <Img
+                src="https://res.kurly.com/kurly/logo/isms_220310.png"
+                alt="isms 로고"
+                width="35px"
+              />
+              <div>
+                [인증범위]마켓컬리 쇼핑몰 서비스 개발 · 운영
+                <br />
+                [유효기간]2022.01.19~2025.01.18
+              </div>
+            </Section>
+
+            <Section>
+              <Img
+                src="https://res.kurly.com/pc/ico/2001/logo_eprivacyplus.png"
+                alt="eprivacy plus 로고"
+                width="35px"
+              />
+              <div margin="0px 10px">
+                개인정보보호 우수 웹사이트 ·<br />
+                개인정보처리시스템인증 (ePRIVACY PLUS)
+              </div>
+            </Section>
+
+            <Section>
+              <Img
+                src="https://res.kurly.com/pc/service/main/2009/logo_payments.png"
+                alt="payments 로고"
+                width="100px"
+              />
+              <div margin="0px 10px">
+                고객님의 안전거래를 위해 현금 등으로 결제 시 저희 쇼핑몰에서
+                가입한
+                <br />
+                토스 페이먼츠 구매안전(에스크로)서비스를 이용하실 수 있습니다.
+              </div>
+            </Section>
+          </OutsideInfo>
+        </Container>
+      </To>
     </>
   );
 };
@@ -166,7 +169,8 @@ const Footer = (props) => {
 export default Footer;
 
 const Wrap = styled.div`
-  margin-left: 30px;
+  /* margin-left: 30px; */
+  margin: 0 auto;
 `;
 
 const KurlyIcon = styled.ul`
@@ -178,6 +182,11 @@ const Bar = styled.span`
   opacity: 20%;
   margin-top: 60px;
   display: block;
+`;
+
+const To = styled.div`
+  margin: 0 auto;
+  width: 100%;
 `;
 
 const CenterInfo = styled.ul`
--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -28,7 +28,7 @@ function App() {
   const is_open_alert = useSelector((state) => state.modal.alert_open);
 
   useEffect(() => {
-    if(baskets.length > 0) {
+    if (baskets.length > 0) {
       dispatch(cartActions.setCart(baskets));
     }
   }, []);
@@ -40,8 +40,8 @@ function App() {
     }
   }, []);
 
-  if(baskets.length) dispatch(cartActions.getCartApi());
-  
+  // if(baskets.length) dispatch(cartActions.getCartApi());
+
   return (
     <>
       <ConnectedRouter history={history}>
